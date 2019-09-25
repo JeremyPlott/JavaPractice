64b423927ec13ebea346c9a25a450790ff52a429
Jeremy <JeremyPlott@gmail.com>
2019-09-25 10:11:02 -0400

    wizard game good enough

--- JavaPractice12.2/src/JavaPracticeApp12_2.java
+++ JavaPractice12.2/src/JavaPracticeApp12_2.java
@@ -1,44 +1,81 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import util.Console;
 
 public class JavaPracticeApp12_2 {
 
 	public static void main(String[] args) {
 		
 		String game = "on";
+		Scanner sc = new Scanner(System.in);
 				
 		System.out.println("Wizard Inventory Game");
 		System.out.println();
-		System.out.println("COMMAND MENU");
-		System.out.println();
-		System.out.println("menu - Show command menu");
-		System.out.println("show - Show all items");
-		System.out.println("grab - Grab an item");
-		System.out.println("edit - Edit an item");
-		System.out.println("drop - Drop an item");
-		System.out.println("exit - Exit program");
 		
-		String[] inv = new String[4]; 
-		String cmd = "menu";
+		List<String> inv = new ArrayList<>();
+		inv.add("Wooden Staff");
+		inv.add("Marauder's Map");
+		inv.add("Wand of Wonder");
 		
-		while (game != "off") {
+		String cmd = Console.getString(getDisplayMenu());
+		System.out.println("Enter your action: ");
+		cmd = sc.next();
+		
+		while (!game.equalsIgnoreCase("off")) {
 			
-			// menu
-			
-			
+			System.out.println();
+			System.out.println("Enter your action: ");
+			cmd = sc.next();
+								
 			// show
-			
+			if (cmd.equalsIgnoreCase("show")) {
+				int invSlot = 1;
+				for (String i: inv) {
+					System.out.println(invSlot+". "+i);
+					invSlot++;
+				}
+			}			
 			
 			// grab
-			
+			else if (cmd.equalsIgnoreCase("grab")) {
+				if (inv.size() > 3) {
+					System.out.println("Your bag is too full");
+				} else {
+					String name = Console.getString("Name: ");
+					inv.add(name);
+					System.out.println("Picked up " + name);
+				}
+			}
 			
 			// edit
-			
+			else if (cmd.equalsIgnoreCase("edit")) {
+				int nbr = Console.getInt("Inv. slot: ");
+				int idxNbr = nbr - 1;
+				String updatedName = Console.getString("New item: ");
+				inv.set(idxNbr, updatedName);
+			}
 			
 			// drop
+			else if (cmd.equalsIgnoreCase("drop")) {
+				int nbr = Console.getInt("Inv. slot: ");
+				int idxNbr = nbr - 1;
+				String dropItem = inv.remove(idxNbr);
+				System.out.println("Dropped " + dropItem + ".");
+			}
 			
-			
-			if (cmd.equalsIgnoreCase("exit")) {				
+			////////////////////////////////
+			// end game
+			else if (cmd.equalsIgnoreCase("exit")) {				
 				game = "off";
 			}
+			
+			else {
+				System.out.println("Invalid command '"+cmd+"'. Task failed successfully!");
+			}
+			
+			cmd = Console.getString(getDisplayMenu());
 			
 		}
 		
@@ -46,4 +83,14 @@ public class JavaPracticeApp12_2 {
 		
 	}
 
+	private static String getDisplayMenu() {
+		String menu = "COMMAND MENU\n" + 
+				"show - Show all items\n" + 
+				"grab - Grab an item\n" + 
+				"edit - Edit an item\n" + 
+				"drop - Drop an item\n" + 
+				"exit - Exit program\n" +
+				"\n";		
+		return menu;
+	}
 }
